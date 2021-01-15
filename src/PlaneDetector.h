3c75389e298ecc0ef5317e1708f2e0fa0e1970fb
Ondrej Vesely <ondrej-vesely@seznam.cz>
2021-01-15 20:55:18 +0100

    Added names and student numbers

--- src/PlaneDetector.h
+++ src/PlaneDetector.h
@@ -1,11 +1,11 @@
 /*
   GEO1015.2020
-  hw03 
+  hw03
   --
-  [YOUR NAME] 
-  [YOUR STUDENT NUMBER] 
-  [YOUR NAME] 
-  [YOUR STUDENT NUMBER] 
+  Ondrej Vesely
+  5162130
+  Guilherme Spinoza Andreo
+  5383994
 */
 
 
